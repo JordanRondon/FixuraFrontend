b9a7c63249a063d244b7a2bacf3e3de38b555594
Pablo AQ <alfaroquispepablo@gmail.com>
2024-09-09 18:43:06 -0500

    Login complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './capaPresentacion/modulos/landing-page/landing-page.component';
+import LoginComponent from "./capaPresentacion/modulos/login/login.component";
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingPageComponent],
+  imports: [RouterOutlet, LandingPageComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './capaPresentacion/modulos/landing-page/landing-page.component';
 import { MuroUsuarioComponent } from './capaPresentacion/modulos/muro-usuario/muro-usuario.component';
 
 export const routes: Routes = [
-    { path:'muro-usuario', component:MuroUsuarioComponent }
+    { path: '', component: LandingPageComponent },
+    { path:'muro-usuario', component:MuroUsuarioComponent },
+    { path:'login', loadComponent: () => import('./capaPresentacion/modulos/login/login.component')}
 ];
