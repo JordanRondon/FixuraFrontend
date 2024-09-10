fb71bec2fd022f2a1decb93d0e4556f1925dc939
JordanRondon <jordanangelrondonpozo@gmail.com>
2024-09-10 13:01:46 -0500

    cambio: mejoras en diseño de post-incidencia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './capaPresentacion/modulos/landing-page/landing-page.component';
 import LoginComponent from "./capaPresentacion/modulos/login/login.component";
 import RegisterComponent from "./capaPresentacion/modulos/register/register.component";
+import { MuroUsuarioComponent } from './capaPresentacion/modulos/muro-usuario/muro-usuario.component';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingPageComponent, LoginComponent, RegisterComponent],
+  imports: [RouterOutlet, LandingPageComponent, LoginComponent, RegisterComponent, MuroUsuarioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
