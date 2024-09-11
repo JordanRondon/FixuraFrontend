557a05042846ca322202e55fc62be079458a36e6
JordanRondon <jordanangelrondonpozo@gmail.com>
2024-09-11 01:27:13 -0500

    correccion: enrutamiento de modulos

--- src/app/capaPresentacion/modulos/landing-page/landing-page.component.ts
+++ src/app/capaPresentacion/modulos/landing-page/landing-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.css'
+  styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent {
 
