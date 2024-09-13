129898ef05b15235842dcb01e261fe6ac14c1fdc
Lolerw <angelus.lolerw@gmail.com>
2024-09-13 18:59:21 -0500

    vista muro-administrador, falta responsividad

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './capaPresentacion/modulos/landing-page/landing-page.component';
 import { MuroUsuarioComponent } from './capaPresentacion/modulos/muro-usuario/muro-usuario.component';
+import { MuroAdministradorComponent } from './capaPresentacion/modulos/muro-administrador/muro-administrador.component';
 
 export const routes: Routes = [
+    
     { path: '', component: LandingPageComponent },
     { path:'muro-usuario', component:MuroUsuarioComponent },
+    { path:'muro-administrador', component:MuroAdministradorComponent},
     { path:'login', loadComponent: () => import('./capaPresentacion/modulos/login/login.component')},
     { path:'register', loadComponent: () => import('./capaPresentacion/modulos/register/register.component')}
 ];
