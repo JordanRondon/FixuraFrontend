ac1f305422692f93a9271a3c0b7dcef39507bacd
Pablo AQ <alfaroquispepablo@gmail.com>
2024-09-15 18:40:33 -0500

    Added the functionality to list Departamentos, Provincias and Distritos in Register

--- src/app/capaPresentacion/modulos/register/register.component.ts
+++ src/app/capaPresentacion/modulos/register/register.component.ts
@@ -1,13 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { DepartamentoServiceService} from '../../../Service/departamento-service.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export default class RegisterComponent {
+export default class RegisterComponent implements OnInit{
 
+  listDepartamento: { idDepart: number, nameDepart: string }[] = [];
+  listProvincia: {idProv : number, nameProv: string, idDepart: number}[] = [];
+  listDistrito: {idDist: number, nameDist: string, idProv: number, idDepart: number}[] = [];
+
+  selectedDepartamento: number | null = null;
+  selectedProvincia: number | null = null;
+
+  constructor(private departamentoService : DepartamentoServiceService){}
+
+  ngOnInit(): void {
+    this.loadDepartamentos()
+  }
+
+  // SE CARGA LOS DEPARTAMENTOS
+  loadDepartamentos(){
+    this.departamentoService.getDepartamento().subscribe(resp => {
+        console.log(resp);
+        if(resp){
+          this.listDepartamento = resp;
+        }
+    });
+  }
+
+  // SE ACTUALIZA LAS PROVINCIAS EN CASO QUE SE ELIJE UN DEPARTAMENTO
+  onDepartamentoChange(event: Event): void {
+    const target = event.target as HTMLSelectElement
+    const idDepart = Number(target.value)
+    if (!isNaN(idDepart)) {
+      this.selectedDepartamento = idDepart;
+      if (this.selectedDepartamento !== null) {
+        this.loadProvincias(idDepart);
+        this.listDistrito = [];
+        this.selectedProvincia = null;
+      }
+    }
+  }
+
+  // SE CARGA LAS PROVINCIAS
+  loadProvincias(idDepart: number): void {
+    this.departamentoService.getProvincia(idDepart).subscribe(resp => {
+      if (resp) {
+        this.listProvincia = resp;
+      }
+    });
+  }
+
+  onProvinciaChange(event : Event): void {
+    const target = event.target as HTMLSelectElement
+    const idProv = Number(target.value)
+    if (!isNaN(idProv)) {
+      this.selectedProvincia = idProv;
+      if (this.selectedDepartamento !== null) {
+        this.loadDistritos(this.selectedDepartamento, idProv);
+      }
+    }
+  }
+
+  // SE CARGA LOS DISTRITOS
+  loadDistritos(idDepart: number, idProv: number): void {
+    this.departamentoService.getDistrito(idDepart, idProv).subscribe(resp => {
+      if (resp) {
+        this.listDistrito = resp;
+      }
+    });
+  }
 }
