21d9e60adfed284ecc5d76662f203f99a59c16db
unknown <kgarcia_m@hotmail.com>
2024-10-10 02:24:40 -0500

    Se arreglo funcion para extraer la direccion al dar un click

--- src/app/capaPresentacion/componentes/registro-incidencia/registro-incidencia.component.ts
+++ src/app/capaPresentacion/componentes/registro-incidencia/registro-incidencia.component.ts
@@ -40,6 +40,7 @@ export class RegistroIncidenciaComponent implements OnInit {
   
   ngOnInit(): void {
     console.log(this.dniUsuario);
+    this.obtenerUbicacionActual();
   }
 
   // Cuando el usuario selecciona una ubicación en el mapa
@@ -51,7 +52,7 @@ export class RegistroIncidenciaComponent implements OnInit {
     }
   }
   //Funcion para extraer la ubicacion actual del usuario
-  /*
+  
   obtenerUbicacionActual(): void {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
@@ -67,25 +68,21 @@ export class RegistroIncidenciaComponent implements OnInit {
       console.error('La geolocalización no es compatible con este navegador.');
     }
   }
-  */
+  
   obtenerDireccion(lat: number, lng: number): void {
-    const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${this.apiKey}`;
-
+    const url = `/api/maps/api/geocode/json?latlng=${lat},${lng}&key=${this.apiKey}`;
+  
     this.http.get(url).subscribe((data: any) => {
       if (data.status === 'OK' && data.results.length > 0) {
         const direccion = data.results[0].formatted_address;
-        //this.ubicacionSeleccionada = direccion;
-        this.formulario.value.ubicacion=direccion
-        this.formulario.patchValue({
-          ubicacion: direccion
-        });
-  
-        console.error(this.formulario.value.ubicacion);
+        this.formulario.patchValue({ ubicacion: direccion });
+        console.log(this.formulario.value.ubicacion);
       } else {
         console.error('Error al obtener la dirección');
       }
     });
   }
+  
   onFileSelected(event: any): void {
     const file = (event.target as HTMLInputElement).files?.[0];
     this.archivoSeleccionado = event.target.files[0];
