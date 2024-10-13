150cdd8f4ba59ca73365790a110a31b6d753c2dd
Paul <paulalexander2909@gmail.com>
2024-10-13 14:33:27 -0500

    Cambios

--- src/app/capaPresentacion/componentes/registro-incidencia/registro-incidencia.component.ts
+++ src/app/capaPresentacion/componentes/registro-incidencia/registro-incidencia.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { CommonModule } from '@angular/common';
 
@@ -12,13 +17,13 @@ import { Incidente } from '../../../Model/Incidente';
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule, GoogleMapsModule],
   templateUrl: './registro-incidencia.component.html',
-  styleUrls: ['./registro-incidencia.component.css']
+  styleUrls: ['./registro-incidencia.component.css'],
 })
 export class RegistroIncidenciaComponent implements OnInit {
   formulario: FormGroup;
   //ubicacionSeleccionada: string = '';
   categorias: string[] = ['Poste', 'Pista', 'Desague'];
-  apiKey: string = 'XIzaSyAu2e7Y6k3AS3Z0olMqdDtI-OdQZB0p44X'; 
+  apiKey: string = 'AIzaSyAu2e7Y6k3AS3Z0olMqdDtI-OdQZB0p44E';
   center: google.maps.LatLngLiteral = { lat: -8.1116, lng: -79.0288 };
   marker: google.maps.Marker | null = null;
   zoom = 17;
@@ -29,15 +34,20 @@ export class RegistroIncidenciaComponent implements OnInit {
 
   @Input() dniUsuario: string | undefined;
 
-  constructor(private fb: FormBuilder,private imagenService: ImagenService,private registroIncideten: IncidenciaService,private http: HttpClient) {
+  constructor(
+    private fb: FormBuilder,
+    private imagenService: ImagenService,
+    private registroIncideten: IncidenciaService,
+    private http: HttpClient
+  ) {
     this.formulario = this.fb.group({
       categoria: ['', Validators.required],
       ubicacion: ['', Validators.required],
       descripcion: ['', Validators.required],
-      foto: ['']
+      foto: [''],
     });
   }
-  
+
   ngOnInit(): void {
     console.log(this.dniUsuario);
   }
@@ -75,11 +85,11 @@ export class RegistroIncidenciaComponent implements OnInit {
       if (data.status === 'OK' && data.results.length > 0) {
         const direccion = data.results[0].formatted_address;
         //this.ubicacionSeleccionada = direccion;
-        this.formulario.value.ubicacion=direccion
+        this.formulario.value.ubicacion = direccion;
         this.formulario.patchValue({
-          ubicacion: direccion
+          ubicacion: direccion,
         });
-  
+
         console.error(this.formulario.value.ubicacion);
       } else {
         console.error('Error al obtener la dirección');
@@ -103,60 +113,66 @@ export class RegistroIncidenciaComponent implements OnInit {
       console.log('dsadasdasd');
       return;
     }
-    
+
     const fechaFormateada = this.obtenerFechaActual();
     const formIncidente = new FormData();
     const formData = new FormData();
     formData.append('file', this.archivoSeleccionado);
-    
+
     // Opcional: enviar otros datos del formulario
     formIncidente.append('fecha_publicacion', fechaFormateada);
     formIncidente.append('descripcion', this.formulario.value.descripcion);
     formIncidente.append('ubicacion', this.formulario.value.ubicacion);
-    formIncidente.append('imagen',"https://raw.githubusercontent.com/KevinGM02/Galeria-Imagenes-Fixura/main/imagenes/"+this.archivoSeleccionado.name );
-    formIncidente.append('total_votos',"0");
-    formIncidente.append('id_estado',"3");
-    formIncidente.append('DNI',this.dniUsuario ?? '');
-    formIncidente.append('id_categoria', this.formulario.value.categoria.toString());
+    formIncidente.append(
+      'imagen',
+      'https://raw.githubusercontent.com/KevinGM02/Galeria-Imagenes-Fixura/main/imagenes/' +
+        this.archivoSeleccionado.name
+    );
+    formIncidente.append('total_votos', '0');
+    formIncidente.append('id_estado', '3');
+    formIncidente.append('DNI', this.dniUsuario ?? '');
+    formIncidente.append(
+      'id_categoria',
+      this.formulario.value.categoria.toString()
+    );
     console.log(fechaFormateada);
     console.log(this.formulario.value.descripcion);
     console.log(this.formulario.value.ubicacion);
     console.log(this.archivoSeleccionado.name);
     console.log(this.dniUsuario);
     console.log(this.formulario.value.categoria.toString());
-    
-    this.imagenService.saveImagenIncidencia(formData).subscribe(resp=>{
-      if(resp){
+
+    this.imagenService.saveImagenIncidencia(formData).subscribe((resp) => {
+      if (resp) {
         console.log('Imagen subida exitosamente');
-        this.registroIncideten.saveIncidencia(formIncidente).subscribe(resp=>{
-          if(resp){
-            this.formulario.reset();
-            this.imagenPrevisualizacion=null
-            console.log('Incidente registrado correctamente');
-          }else{
-            console.error('Error al registrar un incidente');
-          }
-        });
-        
-      }else{
+        this.registroIncideten
+          .saveIncidencia(formIncidente)
+          .subscribe((resp) => {
+            if (resp) {
+              this.formulario.reset();
+              this.imagenPrevisualizacion = null;
+              console.log('Incidente registrado correctamente');
+            } else {
+              console.error('Error al registrar un incidente');
+            }
+          });
+      } else {
         console.error('Error al subir la imagen');
       }
     });
-    
   }
   obtenerFechaActual(): string {
     const fecha = new Date();
-  
+
     const anio = fecha.getFullYear();
     const mes = ('0' + (fecha.getMonth() + 1)).slice(-2); // Los meses en JavaScript son de 0-11
     const dia = ('0' + fecha.getDate()).slice(-2);
-  
+
     const horas = ('0' + fecha.getHours()).slice(-2);
     const minutos = ('0' + fecha.getMinutes()).slice(-2);
     const segundos = ('0' + fecha.getSeconds()).slice(-2);
     const milisegundos = ('00' + fecha.getMilliseconds()).slice(-3);
-  
+
     return `${anio}-${mes}-${dia} ${horas}:${minutos}:${segundos}.${milisegundos}`;
   }
-  
 }
