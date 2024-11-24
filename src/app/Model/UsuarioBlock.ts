f6a84ccaa2e1683efcb21e072c46b2f6491abbf9
Pablo AQ <alfaroquispepablo@gmail.com>
2024-11-24 13:16:14 -0500

    modificación: Se soluciono problema al cargar la foto de perfil del usuario en el apartado Bloquear Usuarios

--- src/app/Model/UsuarioBlock.ts
+++ src/app/Model/UsuarioBlock.ts
@@ -3,19 +3,19 @@ export class UsuarioBlock {
   nombre : string;
   apellido : string;
   correo: string;
-  fotoPerfil : string;
+  foto_perfil : string;
 
   constructor(
     dni : string,
     nombre : string,
     apellido : string,
     correo: string,
-    fotoPerfil : string
+    foto_perfil : string
   ){
     this.dni = dni;
     this.nombre = nombre;
     this.apellido = apellido;
     this.correo = correo;
-    this.fotoPerfil = fotoPerfil;
+    this.foto_perfil = foto_perfil;
   }
 }
